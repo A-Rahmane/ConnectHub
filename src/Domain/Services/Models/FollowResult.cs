016fda5e551fb8dd11adf20f23fb947e26429568
Menouer Abderrahmane <menouer.a.rahmane@gmail.com>
2025-09-14 01:13:52 +0100

    Todo tasks updated for the Domain Layer

---
+++ src/Domain/Services/Models/FollowResult.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enums;
+
+namespace Domain.Services.Models
+{
+    public class FollowResult
+    {
+        public bool IsSuccessful { get; set; }
+        public FollowStatus Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ src/Domain/Services/Models/ModerationResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Services.Models
+{
+    public class ModerationResult
+    {
+        public bool IsApproved { get; set; }
+        public string? Reason { get; set; }
+        public double ConfidenceScore { get; set; }
+    }
+}
